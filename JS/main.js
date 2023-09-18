b7f0e990b47fe2db5af21eb387c695d7498eefe4
Roberto0611 <ochoacuevasr@gmail.com>
2023-09-18 09:19:08 -0700

    Some detail related to the catalog button

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,8 @@
 //Made by: Roberto Ochoa Cuevas:
 
+//Catalog Button
+catalogButton = document.getElementById('catalogButton');
+
+catalogButton.addEventListener('click', function(){
+    window.location.href = '../HTML/catalog.html';
+})
