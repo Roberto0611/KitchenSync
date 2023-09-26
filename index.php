b1744d591640f22ae6846af9237b64ccf2c08d97
Roberto0611 <ochoacuevasr@gmail.com>
2023-09-25 19:03:13 -0700

    change the structure to tables

--- index.php
+++ index.php
@@ -9,9 +9,7 @@
 </head>
 <body>
     
-    <!--Conexion PHP-->
-    
-
+   
     <header>
         <nav>
             <ul>
@@ -29,31 +27,55 @@
         <button>Buscar</button>
     </div>
     <div class="product-container">
-        <ul class="product-list">
-            <!-- Producto 1 -->
-            <li class="product-item">
-                <div class="product-info">
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Fwalmart-labs%2Fimage%2Fupload%2Fw_960%2Cdpr_auto%2Cf_auto%2Cq_auto%3Abest%2Fgr%2Fimages%2Fproduct-images%2Fimg_large%2F00750102051534L.jpg&f=1&nofb=1&ipt=0e4b809e764adf3356aaba6934950f58e308a6c986365ae147adfcde4dd24d35&ipo=images" alt="Producto 1" class="product-image">
-                    <div>
-                        <h3>Leche Lala</h3>
-                        <p>Descripción del Producto 1...</p>
+        <table class="product-list" width="100%" cellspacing="0">
+            <!-- Producto 3 -->
+            <tr>
+                <td>
+                    <div class="product-item">
+                        <div class="product-info">
+                            <table style="width: 100%;">
+                                <tr>
+                                    <td style="vertical-align: middle;">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fres.cloudinary.com%2Fwalmart-labs%2Fimage%2Fupload%2Fw_960%2Cdpr_auto%2Cf_auto%2Cq_auto%3Abest%2Fgr%2Fimages%2Fproduct-images%2Fimg_large%2F00750102051534L.jpg&f=1&nofb=1&ipt=0e4b809e764adf3356aaba6934950f58e308a6c986365ae147adfcde4dd24d35&ipo=images" alt="Producto 4">
+                                    </td>
+                                    <td style="vertical-align: middle;">
+                                        <div class="product-text">
+                                            <h3>Leche Lala</h3>
+                                            <p>Descripción del Producto 3...</p>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <button class="delete-button">-</button>
                     </div>
-                </div>
-                <button class="delete-button">-</button>
-            </li>
-            <!-- Producto 2 -->
-            <li class="product-item">
-                <div class="product-info">
-                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fplayatampa.com%2Fwp-content%2Fuploads%2Fsites%2F78%2F2019%2F07%2Fshutterstock_575528746-1024x778.jpg&f=1&nofb=1&ipt=d5556cf58ceedb3f130f951bad8297a36f023d01097d324d76a8e09a8414ca41&ipo=images" alt="Producto 2" class="product-image">
-                    <div>
-                        <h3>Platanos</h3>
-                        <p>Descripción del Producto 2...</p>
+                </td>
+            </tr>
+            <!-- Producto 4 -->
+            <tr>
+                <td>
+                    <div class="product-item">
+                        <div class="product-info">
+                            <table style="width: 100%;">
+                                <tr>
+                                    <td style="vertical-align: middle;">
+                                    <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fplayatampa.com%2Fwp-content%2Fuploads%2Fsites%2F78%2F2019%2F07%2Fshutterstock_575528746-1024x778.jpg&f=1&nofb=1&ipt=d5556cf58ceedb3f130f951bad8297a36f023d01097d324d76a8e09a8414ca41&ipo=images" alt="Producto 2">
+                                    </td>
+                                    <td style="vertical-align: middle;">
+                                        <div class="product-text">
+                                            <h3>Platanos</h3>
+                                            <p>Descripción del Producto 4...</p>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                        <button class="delete-button">-</button>
                     </div>
-                </div>
-                <button class="delete-button"> - </button>
-            </li>
+                </td>
+            </tr>
             <!-- Puedes agregar más productos aquí -->
-        </ul>
+        </table>
     </div>
 
     <!----------MODAL Y SU BOTON---------->
